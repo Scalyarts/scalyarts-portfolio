8c596b8266cb643a668cad964c0f3da9a9d383d1
Scalyarts <scalysound@gmail.com>
2025-07-04 18:57:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+
+const PortfolioSite = dynamic(() => import('../components/PortfolioSite'), { ssr: false })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ScalyArts – 3D Художник</title>
+        <meta name="description" content="3D модели мебели и интерьеров от ScalyArts" />
+      </Head>
+      <PortfolioSite />
+    </>
+  )
+}
